dadb40173654f105da4fe5eadd037141f6a8839c
WoundTight <dirtmodified@gmail.com>
2015-02-09 23:29:34 -0600

    Create trltetmo Beanstalk device tree

--- cm.mk
+++ cm.mk
@@ -17,10 +17,10 @@
 $(call inherit-product, device/samsung/trltetmo/full_trltetmo.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/beanstalk/config/nfc_enhanced.mk)
 
 # Inherit common CM phone.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/beanstalk/config/common_full_phone.mk)
 
 PRODUCT_DEVICE := trltetmo
-PRODUCT_NAME := cm_trltetmo
+PRODUCT_NAME := bs_trltetmo
